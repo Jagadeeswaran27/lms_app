489e4b82fa9b49de87c60f1c5c9515549915fa0f
Balaji Venkatraman <balaji@moonraft.com>
2024-08-27 17:38:15 +0530

    push registration for students

--- micro_apps/registration_app/lib/core/services/registration/registration_service.dart
+++ micro_apps/registration_app/lib/core/services/registration/registration_service.dart
@@ -1,8 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:registration_app/models/registration/course_model.dart';
+import 'package:registration_app/models/registration/student_registration_model.dart';
 import 'package:registration_app/models/registration/teacher_registration_model.dart';
 
-class TeacherRegistrationService {
+class RegistrationService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<String> registerTeacher({
@@ -31,6 +32,62 @@ class TeacherRegistrationService {
 
       // Create TeacherRegistrationModel
       final TeacherRegistrationModel registration = TeacherRegistrationModel(
+        courseName: course.courseTitle,
+        courseId: course.courseId,
+        registrationId: registrationId,
+        registeredBy: registeredBy,
+        status: 'Pending',
+        paymentStatus: 'Unpaid',
+        imageUrl: course.imageUrl,
+        registeredFor: 'self',
+      );
+
+      // Store the registration in Firestore
+      await _firestore
+          .collection('institutes')
+          .doc(instituteId)
+          .collection('teachers-registrations')
+          .doc(registrationId)
+          .set(registration.toJson());
+
+      // Update the user's registered courses in Firestore
+      await _firestore.collection('lms-users').doc(registeredBy).update({
+        'registeredCourses': FieldValue.arrayUnion([course.courseId])
+      });
+
+      return registrationId;
+    } catch (e) {
+      print('Error registering teacher: $e');
+      throw Exception('Failed to register teacher');
+    }
+  }
+
+  Future<String> registerStudent({
+    required CourseModel course,
+    required String selectedBatchDay,
+    required String selectedBatchTime,
+    required String registeredBy,
+  }) async {
+    try {
+      // Get the first institute ID
+      final QuerySnapshot instituteSnapshot =
+          await _firestore.collection('institutes').limit(1).get();
+      if (instituteSnapshot.docs.isEmpty) {
+        return '';
+      }
+
+      final String instituteId = instituteSnapshot.docs.first.id;
+
+      // Generate a unique registration ID
+      final DocumentReference registrationRef = _firestore
+          .collection('institutes')
+          .doc(instituteId)
+          .collection('students-registrations')
+          .doc();
+      final String registrationId = registrationRef.id;
+
+      // Create TeacherRegistrationModel
+      final StudentRegistrationModel registration = StudentRegistrationModel(
         courseName: course.courseTitle,
         courseId: course.courseId,
         registrationId: registrationId,
